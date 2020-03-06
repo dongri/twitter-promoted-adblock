2c83f5f4659e927025d8c296626be8e509a753b3
Dongri Jin <dongrify@gmail.com>
2020-03-06 20:56:14 +0900

    Initial commit

---
+++ block.js
@@ -0,0 +1,14 @@
+
+var block = function(){
+  var words = ["Promoted", "プロモーション", "推荐", "프로모션"];
+  words.forEach(function(text){
+    var elem = null;
+    $("span:contains('" + text + "')").each(function(){
+        if($(this).text() == text) elem = $(this);
+    });
+    if (elem != null){
+      elem.parent().parent().parent().parent().parent().parent().parent().parent().parent().css("display", "none");
+    }
+  });
+}
+setInterval(block, 1000);
